d9ecb097dfce1d0862c7e4c9813b1c1fc560ee22
derob357 <derob357@yahoo.com>
2014-04-09 08:48:41 -0500

    trackDB and HomeController adds

--- EntityLINQ/Controllers/HomeController.cs
+++ EntityLINQ/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EntityLINQ.Data;
+using EntityLINQ.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +12,19 @@ namespace EntityLINQ.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            ApplicationDbContext db = new ApplicationDbContext();
+            MyVM bucket = new MyVM();
+            bucket.PracticeQuery1 = db.Customers.ToList();
+            bucket.PracticeQuery2 = db.Orders.ToList();
+            bucket.PracticeQuery3 = db.Orders.Where(p => p.Description.Contains("A")).ToList();
+            bucket.PracticeQuery4 = db.Orders.Where(o => o.OrderDate.Month > o.OrderDate.Month-6)
+                .Where(y => y.OrderDate.Year == 2014)
+                .ToList();
+            //bucket.PracticeQuery5 = db.Customers.ToList().Find();
+            //bucket.PracticeQuery5 = db.Customers.Where(x => x.Id.ToList().Find(5));
+           // bucket.PracticeQuery6 = db.Customers.ToList().Skip(); 
+          //  bucket.PracticeQuery7 = db.Customers.ToList().OrderByDescending(Customer);
+            return View(bucket);
         }
 
         public ActionResult About()
---
+++ EntityLINQ/Models/MyVM.cs
@@ -0,0 +1,28 @@
+﻿using EntityLINQ.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EntityLINQ.Models
+{
+    public class MyVM
+    {
+       
+        public List<Customer> PracticeQuery1 { get; set; }
+        public List<Order> PracticeQuery2 { get; set; }
+        public List<Order> PracticeQuery3 { get; set; }
+        public List<Order> PracticeQuery4 { get; set; }
+        public List<Customer> PracticeQuery5 { get; set; }
+        public List<Customer> PracticeQuery6 { get; set; }
+        public List<Customer> PracticeQuery7 { get; set; }
+        public List<Order> PracticeQuery8 { get; set; }
+        public List<Customer> PracticeQuery9 { get; set; }
+        public List<Order> PracticeQuery10 { get; set; }
+        public List<Customer> PracticeQuery11 { get; set; }
+        public List<Customer> PracticeQuery12 { get; set; }
+        public List<Order> PracticeQuery13 { get; set; }
+    
+
+    }
+}
